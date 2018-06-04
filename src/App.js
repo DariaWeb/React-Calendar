af222add850403e1a7dc5335555ce72c9ccb43b3
DariaWeb <voloshko.da@gmail.com>
2018-06-04 19:30:11 +1200

    react calendar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Calendar from './components/Calendar';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <div id="logo">
+        <span className="icon">date_range</span>
+        <span>react<b>calendar</b></span>
+        </div>
+      </header>
+      <main>
+        <Calendar />
+      </main> 
+      </div>
+    );
+  }
+}
+
